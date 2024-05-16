828928c28507adbaf3145d21c5a6524afcc43933
Rabby <mdrabbybd4747@gmail.com>
2024-05-16 23:41:56 +0600

    create schema and model with mongoos

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv"
-import path from "path"
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), "env") })
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-	path: process.env.PORT,
-	database_url: process.env.DATABASE_URL,
-}
+  path: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
