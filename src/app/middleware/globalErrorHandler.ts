e31399a747d75e92b06d06f19b23836bf475af61
Rabby <mdrabbybd4747@gmail.com>
2024-06-04 01:30:33 +0600

    convert mongoose default errors

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,38 +2,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ErrorRequestHandler } from 'express';
-import { ZodError, ZodIssue } from 'zod';
-import { TErrorSource } from '../interface/error';
+import { ZodError } from 'zod';
 import config from '../config';
+import handleZodError from '../error/handleZodError';
+import { TErrorSources } from '../interface/error';
+import handleValidatorError from '../error/handleValidatorError';
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statuscode = err.statusCode || 500;
   let message = err.message || 'something went wrong';
 
-  let errorSources: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'something went wrong',
     },
   ];
 
-  const handleZodError = (err: ZodError) => {
-    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue.message,
-      };
-    });
-
-    const statusCode = 400;
-    return {
-      statusCode,
-      message: 'Validation Error',
-      errorSources,
-    };
-  };
-
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
+
+    statuscode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidatorError(err);
 
     statuscode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
