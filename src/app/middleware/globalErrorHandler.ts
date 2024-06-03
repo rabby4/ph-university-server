7f68218b3350da17557163dd29508fab8464a1ef
Rabby <mdrabbybd4747@gmail.com>
2024-06-04 02:11:55 +0600

    handle duplicate key value error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -8,6 +8,7 @@ import handleZodError from '../error/handleZodError';
 import { TErrorSources } from '../interface/error';
 import handleValidatorError from '../error/handleValidatorError';
 import handleCastError from '../error/handleCastError';
+import handleDuplicateError from '../error/handleDuplicateError';
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statuscode = err.statusCode || 500;
   let message = err.message || 'something went wrong';
@@ -27,23 +28,25 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidatorError(err);
-
     statuscode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'CastError') {
     const simplifiedError = handleCastError(err);
-
+    statuscode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
     statuscode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   }
-
   return res.status(statuscode).json({
     success: false,
     message,
     errorSources,
-    // err,
+    err,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
